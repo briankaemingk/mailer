920f57042fa77eda36a706753dd271818ab87de5
briankaemingk <brian.e.k@gmail.com>
2015-08-19 11:48:07 -0700

    Working filter through all inbox items and archive FROM1 bill arrived messages

--- server2.js
+++ server2.js
@@ -27,8 +27,7 @@ var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         xoauth2: xoauth2gen
-    },
-    debug: true
+    }
 });
 
 function sendMail(message) {
@@ -44,11 +43,7 @@ function sendMail(message) {
         if (error) {
             return console.log(error);
         }
-        console.log('Message sent: ' + info.response);
-
-        //info.once("sent", function(data){
-        //    console.log("Message was accepted by %s", data.domain);
-        //});
+        console.log('Message sent: <' + mail_opts.subject + '>');
     });
 }
 
@@ -62,10 +57,11 @@ function accessImap(token) {
     });
 
     function openInbox(cb) {
-        imap.openBox('INBOX', true, cb);
+        imap.openBox('INBOX', false, cb);
     }
 
     imap.once('ready', function () {
+
         openInbox(function (err, box) {
                 if (err) throw err;
                 //console.log('Inbox has ' + box.messages.total + ' messages');
@@ -73,7 +69,7 @@ function accessImap(token) {
                 //If the inbox has more than one message in it
                 if (box.messages.total === 0) {
                     console.log('Inbox has 0 messages');
-                    imap.close();
+                    imap.end();
                 }
                 else {
                     var messages = [];
@@ -113,9 +109,10 @@ function accessImap(token) {
                             )
                             ;
                         });
-                        //msg.once('attributes', function (attrs) {
-                        //    console.log('Attributes: %s', inspect(attrs, false, 8));
-                        //});
+                        msg.once('attributes', function (attrs) {
+                            //console.log('Attributes: %s', inspect(attrs));
+                            messages[seqno].attributes = attrs;
+                        });
                         //msg.once('end', function () {
                         //    console.log('Finished');
                         //});
@@ -130,25 +127,32 @@ function accessImap(token) {
 
                         messages.forEach(function (message) {
 
-                            //if the message is from:FROM1 and the subject matches a string that starts with:
-                            //"Notification - Your " and ends with " bill has arrived"
-                            if (process.env.FROM1 === message.header.from.toString() && from1_patt_sub.test(message.header.subject.toString())) {
-                                console.log(message.header.subject.toString() + ' MATCHES');
+                                //if the message is from:FROM1 and the subject matches a string that starts with:
+                                //"Notification - Your " and ends with " bill has arrived"
+                                if (process.env.FROM1 === message.header.from.toString() && from1_patt_sub.test(message.header.subject.toString())) {
+                                    console.log('Email with subject: <' + message.header.subject.toString() + '> recognized as a newly arrived bill');
 
-                                //Get bank
-                                var bank = message.header.subject.toString().match(from1_patt_sub)[1];
+                                    imap.setFlags(message.attributes.uid, '\Deleted', function (err) {
+                                        if (err)
+                                            console.log(err);
+                                    });
 
-                                //Finds the dollar amount and the due date of the payment
-                                var from1_patt_body = /Your payment for (\$[0-9,.]+) from CHECKING is scheduled for ([0-9\/]+)/;
-                                var bill_amount = message.body.match(from1_patt_body)[1];
-                                var payment_date = message.body.match(from1_patt_body)[2];
-                                //console.log('bill amt '+ bill_amount + ' payment date ' + payment_date);
-                                var subject = payment_date + ' - ' + bill_amount + ' ' + bank + ' bill <pgen>';
-                                console.log(subject);
-                                send_messages.push({subject: subject, body: message.body});
+
+                                    //Get bank
+                                    var bank = message.header.subject.toString().match(from1_patt_sub)[1];
+
+                                    //Finds the dollar amount and the due date of the payment
+                                    var from1_patt_body = /Your payment for (\$[0-9,.]+) from CHECKING is scheduled for ([0-9\/]+)/;
+                                    var bill_amount = message.body.match(from1_patt_body)[1];
+                                    var payment_date = message.body.match(from1_patt_body)[2];
+                                    //console.log('bill amt '+ bill_amount + ' payment date ' + payment_date);
+                                    var subject = payment_date + ' - ' + bill_amount + ' ' + bank + ' bill <pgen>';
+                                    send_messages.push({subject: subject, body: message.body});
+                                }
+
                             }
-
-                        });
+                        )
+                        ;
 
                         if (send_messages.length !== 0) {
                             send_messages.forEach(function (message) {
