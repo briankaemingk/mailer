662d964e17153721e8c3e884cdb32be2db6209da
briankaemingk <brian.e.k@gmail.com>
2015-08-20 13:36:16 -0700

    Started to implement WF sweep

--- server2.js
+++ server2.js
@@ -5,7 +5,8 @@ var nodemailer = require('nodemailer');
 var xoauth2gen = require('xoauth2');
 var Imap = require('imap');
 var inspect = require('util').inspect;
-var imap;
+var inboxImap;
+var wfImap;
 var transporter;
 
 //Initialize xoauth2 generator
@@ -27,12 +28,12 @@ transporter = nodemailer.createTransport({
 //Get login token
 xoauth2gen.getToken(initializeImap);
 
-//Initialize imap object
-function initializeImap(err, token){
+//Initialize inboxImap objects
+function initializeImap(err, token) {
     if (err) {
         return console.log(err);
     }
-    imap = new Imap({
+    inboxImap = new Imap({
         xoauth2: token,
         host: 'imap.gmail.com',
         port: 993,
@@ -40,47 +41,57 @@ function initializeImap(err, token){
         keepalive: true
     });
 
-    imap.connect();
-    imap.once('ready', runOnceConnected);
+    wfImap = new Imap({
+        xoauth2: token,
+        host: 'imap.gmail.com',
+        port: 993,
+        tls: true,
+        keepalive: true
+    });
 
-    imap.once('error', function (err) {
+    inboxImap.connect();
+    inboxImap.once('ready', scanInboxforFROM1NewBill);
+    inboxImap.once('error', function (err) {
         console.log(err);
     });
-
-    imap.once('end', function () {
+    inboxImap.once('end', function () {
         console.log('Connection ended');
     });
-}
 
-function runOnceConnected(){
-    scanInboxforFROM1NewBill();
-    scanWFforPaymentsMade();
+    wfImap.connect();
+    wfImap.once('ready', scanWFforPaymentsMade);
+    wfImap.once('error', function (err) {
+        console.log(err);
+    });
+    wfImap.once('end', function () {
+        console.log('Connection ended');
+    });
+
 }
 
 //Checks if inbox has any messages in it.
 //If it does -> go through every message and looks for a FROM1 newly arrived bill
 //If it doesn't -> wait for new messages and go through those if they exist
 function scanInboxforFROM1NewBill() {
-
-    imap.openBox('INBOX', false, function (err, box) {
+    inboxImap.openBox('INBOX', false, function (err, box) {
         if (err) throw err;
-        //console.log('Inbox has ' + box.messages.total + ' messages');
 
-        //If the inbox has more than one message in it
+        //If the box is empty
         if (box.messages.total === 0) {
-            //console.log('Inbox has 0 messages');
 
-            imap.once('mail', function (num) {
+            //Listen for new mail
+            inboxImap.once('mail', function (num) {
                 console.log(num + ' new message');
-                filterFROM1();
+                scanInboxforFROM1NewBill();
             });
         }
-        else {
 
+        //If the box has messages in it
+        else {
             var messages = [];
 
             //For every message in the inbox
-            var f = imap.seq.fetch('1:' + box.messages.total, {
+            var f = inboxImap.seq.fetch('1:' + box.messages.total, {
                     bodies: ['HEADER.FIELDS (TO FROM SUBJECT)', 'TEXT']
                 })
                 ;
@@ -133,6 +144,8 @@ function scanInboxforFROM1NewBill() {
 
                 //Go through all messages in the inbox
                 messages.forEach(function (message) {
+                        //console.log('FROM1: ' + process.env.FROM1);
+                        //console.log('Email from: ' + message.header.from.toString());
 
                         //if the message is from:FROM1
                         // AND the subject matches a string that starts with: "Notification - Your " and ends with " bill has arrived"
@@ -143,7 +156,7 @@ function scanInboxforFROM1NewBill() {
                         ) {
                             console.log('Email with subject: <' + message.header.subject.toString() + '> recognized as a newly arrived bill');
 
-                            imap.setFlags(message.attributes.uid, '\Deleted', function (err) {
+                            inboxImap.setFlags(message.attributes.uid, '\Deleted', function (err) {
                                 if (err)
                                     console.log(err);
                             });
@@ -162,24 +175,139 @@ function scanInboxforFROM1NewBill() {
                     }
                 );
 
-                //imap.end();
+                //inboxImap.end();
 
-                imap.once('mail', function (num) {
+                inboxImap.once('mail', function (num) {
                     console.log(num + ' new message');
                     scanInboxforFROM1NewBill();
                 });
 
-                //imap.end();
+                //inboxImap.end();
             });
         }
     });
-};
+
+}
 
 //Go through every message in the @WaitingFor mailbox.
 //Deleting the @WaitingFor label for when the payment will be posted/withdrawn
-function scanWFforPaymentsMade(){
+function scanWFforPaymentsMade() {
+    wfImap.openBox('@WAITING FOR', false, function (err, box) {
+        if (err) throw err;
 
-};
+            //If the box is empty
+            if (box.messages.total === 0) {
+
+                //Listen for new mail
+                wfImap.once('mail', function (num) {
+                    console.log(num + ' new message');
+                    scanWFforPaymentsMade();
+                });
+            }
+
+            //If the box has messages in it
+            else {
+
+                var messages = [];
+
+                //For every message in the inbox
+                var f = wfImap.seq.fetch('1:' + box.messages.total, {
+                        bodies: ['HEADER.FIELDS (TO FROM SUBJECT)']
+                    })
+                    ;
+
+                f.on('message', function (msg, seqno) {
+                    //console.log('Message #%d', seqno);
+                    //var prefix = '(#' + seqno + ') ';
+                    var message = {};
+                    messages[seqno] = message;
+
+                    msg.on('body', function (stream, info) {
+                        var buffer = '', count = 0;
+                        var header;
+                        stream.on('data', function (chunk) {
+                            count += chunk.length;
+                            buffer += chunk.toString('utf8');
+                            //    if (info.which === 'TEXT')
+                            //        console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+                        });
+                        stream.once('end', function () {
+                                if (info.which !== 'TEXT') {
+                                    header = Imap.parseHeader(buffer);
+                                    messages[seqno].header = header;
+                                }
+                                else {
+                                    messages[seqno].body = buffer;
+                                    //console.log(buffer);
+                                }
+                            }
+                        )
+                        ;
+                    });
+                    msg.once('attributes', function (attrs) {
+                        //console.log('Attributes: %s', inspect(attrs));
+                        messages[seqno].attributes = attrs;
+                    });
+                    //msg.once('end', function () {
+                    //    console.log('Finished');
+                    //});
+                });
+
+                f.once('error', function (err) {
+                    console.log('Fetch error: ' + err);
+                });
+
+                f.once('end', function () {
+                    //Matches anything that starts with "Notification - Your " and ends with " bill has arrived"
+                    var from1_patt_sub_bill_arrived = /^Notification - Your (.*)(?= bill has arrived)/;
+                    //Finds the dollar amount and the due date of the payment
+                    var from1_patt_body = /Your payment for (\$[0-9,.]+) from CHECKING is scheduled for ([0-9\/]+)/;
+
+                    console.log('Message count: ' + messages.length);
+
+                    //Go through all messages in the inbox
+                    messages.forEach(function (message) {
+
+                            //if the message is from:USER
+                            // AND the subject matches a string that starts with: "Notification - Your " and ends with " bill has arrived"
+                            // AND the body has a scheduled payment amount
+                            if (process.env.USER === message.header.from.toString()
+                                && from1_patt_sub_bill_arrived.test(message.header.subject.toString())
+                                && from1_patt_body.test(message.body)
+                            ) {
+                                console.log('Email with subject: <' + message.header.subject.toString() + '> recognized as a newly arrived bill');
+
+                                wfImap.setFlags(message.attributes.uid, '\Deleted', function (err) {
+                                    if (err)
+                                        console.log(err);
+                                });
+
+
+                                //Get bank
+                                var bank = message.header.subject.toString().match(from1_patt_sub_bill_arrived)[1];
+                                var bill_amount = message.body.match(from1_patt_body)[1];
+                                var payment_date = message.body.match(from1_patt_body)[2];
+                                //console.log('bill amt '+ bill_amount + ' payment date ' + payment_date);
+                                var subject = payment_date + ' - ' + bill_amount + ' ' + bank + ' bill <pgen>';
+                                sendMail({subject: subject, body: message.body});
+                            }
+
+
+                        }
+                    );
+
+
+
+                    wfImap.once('mail', function (num) {
+                        console.log(num + ' new message');
+                        scanInboxforFROM1NewBill();
+                    });
+
+                });
+            }
+    });
+}
+
 
 //Takes a message object with a subject:"" and body:"" and sends the messsage from and to USER
 function sendMail(message) {
