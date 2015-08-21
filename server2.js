c97c3c9ca98e480896e2144f8351e644f7a47061
briankaemingk <brian.e.k@gmail.com>
2015-08-20 21:27:33 -0700

    Scan bdn30 works for generic payment type

--- server2.js
+++ server2.js
@@ -189,8 +189,7 @@ function scanInboxforFROM1NewBill() {
 
 }
 
-//Go through every message in the @WaitingFor mailbox.
-//Deleting the @WaitingFor label for when the payment will be posted/withdrawn
+//Sweeps through the bdn30 mailbox, deleting the bdn30 label when the payment posted date is yesterday
 function scanbdn30forPaymentsMade() {
     bdn30Imap.openBox('Bills due (30 days)', false, function (err, box) {
         if (err) throw err;
@@ -258,16 +257,32 @@ function scanbdn30forPaymentsMade() {
             });
 
             f.once('end', function () {
-                //Matches anything that starts with "Notification - Your " and ends with " bill has arrived"
+                //Matches the payment date and the dollar amount of the subject
                 var bdn30_patt_sub = /^Notification: ([0-9\/]+) - (\$[0-9,.]+).*/;
+
+                //Matches a 00/xx/0000 pattern
+                var bdn30_patt_generic_date = /00\/([0-9][0-9])\/0000/;
+
+                //Matches a date sent pattern
+                var bdn30_patt_date_sent = /^[a-z]+ ([a-z]+)/i;
 
                 //Go through all messages in the inbox
                 messages.forEach(function (message) {
 
                         var subject = message.header.subject.toString();
+                        var date_sent = message.attributes.date.toString();
+                        //console.log('date sent: ' + date_sent);
+                        var month_sent_name = date_sent.match(bdn30_patt_date_sent)[1];
+                        var month_sent_num = addLeadingZero(convertMonthNameToNumber(month_sent_name).toString());
+                        //console.log('month sent num: ' + month_sent_num);
+
+                        var d = new Date();
+                        var last_month = addLeadingZero(new String(d.getMonth()));
+                        //console.log('last month: ' + last_month);
 
                         var payment_date = subject.match(bdn30_patt_sub)[1];
                         var bill_amount = subject.match(bdn30_patt_sub)[2];
+
                         //console.log('bill amt ' + bill_amount + ' payment date ' + payment_date);
 
                         //If the payment was processed yesterday, archive the message
@@ -278,6 +293,24 @@ function scanbdn30forPaymentsMade() {
                                 console.log('Payment made: <' + subject +'>');
                             });
                         }
+
+                        //If the date is in a generic payment days in the format 00/xx/0000
+                        else if(bdn30_patt_generic_date.test(payment_date)){
+                            var generic_payment_day = payment_date.match(bdn30_patt_generic_date)[1];
+                            //console.log('generic payment day: ' + generic_payment_day);
+                            var yesterday = addLeadingZero(new String(d.getDate()) - 1);
+                            //console.log('yesterday: '+ yesterday);
+
+                            //If payment date was yesterday and the month the email was sent was last month, archive the message
+                            if(generic_payment_day === yesterday && month_sent_num === last_month) {
+                                bdn30Imap.setFlags(message.attributes.uid, '\Deleted', function (err) {
+                                    if (err)
+                                        console.log(err);
+                                    console.log('Payment made: <' + subject +'>');
+                                });
+                            }
+                        }
+
                     }
                 );
 
@@ -325,4 +358,10 @@ function addLeadingZero(string) {
     if (string.length === 1)
         string = "0" + string;
     return string;
+}
+
+function convertMonthNameToNumber(monthName) {
+    var myDate = new Date(monthName + " 1, 2000");
+    var monthDigit = myDate.getMonth();
+    return isNaN(monthDigit) ? 0 : (monthDigit + 1);
 }
